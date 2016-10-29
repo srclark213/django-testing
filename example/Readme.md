d846b03e7abfb6b6b47851985afcfcef5ed9a7b8
srclark <stephen.r.clark@hsutx.edu>
2016-10-29 18:55:21 -0500

    moved tutorial code to repo, added readme to apps

---
+++ example/Readme.md
@@ -0,0 +1,4 @@
+# Example
+### Written following the djangoproject.com tutorial
+
+[Go to tutorial](https://docs.djangoproject.com/en/1.10/intro/tutorial01/)
---
+++ example/polls/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+   	# /polls/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # /polls/5/
+    url(r'^specifics/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # /polls/5/results/
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    # /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+
+    	# # /polls/
+ #    url(r'^$', views.index, name='index'),
+ #    # /polls/5/
+ #    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+ #    # /polls/5/results/
+ #    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+]
