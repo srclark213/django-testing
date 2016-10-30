dadaa1c76a5a923487c9a0166f0443fea54903cf
srclark <stephen.r.clark@hsutx.edu>
2016-10-29 19:12:07 -0500

    got hello world going

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todoList.apps.TodolistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
