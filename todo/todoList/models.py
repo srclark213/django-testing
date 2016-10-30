6f54f811c57e175e4c6dda4c13bab0da5b46d7f3
srclark <stephen.r.clark@hsutx.edu>
2016-10-29 19:40:33 -0500

    got basic db and admin stuff setup

--- todo/todoList/models.py
+++ todo/todoList/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class List(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    creationDate = models.DateTimeField('creation date')
+    archive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class ListItem(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500, blank=True, null=True)
+    completion = models.BooleanField(default=False)
+    order = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.text
