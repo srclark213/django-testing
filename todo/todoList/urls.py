2e49eb60c67641177251497c2c9d3a0e6a81fa73
srclark <stephen.r.clark@hsutx.edu>
2016-10-30 19:46:17 -0500

    simple structure setup with a few REST endpoints

--- todo/todoList/urls.py
+++ todo/todoList/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'todoList'
 
 urlpatterns = [
-    # /lists/
+    # /
     url(r'^$', views.index, name='index'),
+    # /lists/
+    url(r'^lists/$', views.allLists, name='lists'),
+    # /lists/{pk}
+    url(r'^lists/(?P<list_id>[0-9]+)', views.oneList, name='list'),
 ]
--- todo/todoList/views.py
+++ todo/todoList/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.core import serializers
+
+from .models import List
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'todoList/home.html')
+
+def oneList(request, list_id):
+    data = serializers.serialize('json', [List.objects.get(pk=list_id),])
+    return HttpResponse(data, content_type="application/json")
+
+def allLists(request):
+    data = serializers.serialize('json', List.objects.all())
+    return HttpResponse(data, content_type="application/json")
